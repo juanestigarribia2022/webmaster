91ea24be30ee3733bc19a173f9c24b052ed6b394
Juan Esteban Estigarribia <estigarribiajuanesteban@gmail.com>
2022-12-19 11:01:17 -0300

    ELIMINAR,MODIFICAR ADMIN/NOVEDADES

--- SITIO FINAL/models/novedadesModel.js
+++ SITIO FINAL/models/novedadesModel.js
@@ -6,9 +6,36 @@ async function getNovedades() {
         return rows;
 }
 async function deleteNovedadesById(id) {
-        var query ="delete from novedades where id = ?";
+        var query = "delete from novedades where id = ?";
         var rows = await pool.query(query, [id]);
         return rows;
 }
 
-module.exports = { getNovedades, deleteNovedadesById };
+async function insertNovedades(obj) {
+        try {
+                var query = "insert into novedades set ?";
+                var rows = await pool.query(query, [obj])
+                return rows;
+        } catch (error) {
+                console.log(error);
+                throw error;
+        }
+}
+
+async function getNovedadesById(id) {
+        var query = 'select * from novedades where id = ?';
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+}
+
+async function modificarNovedadesById(obj, id) {
+        try {
+                var query = 'update novedades set ? where id=?';
+                var rows = await pool.query(query, [obj, id]);
+                return rows;
+        } catch (error) {
+                throw error;
+        }
+}
+
+module.exports = { getNovedades, deleteNovedadesById, insertNovedades, getNovedadesById, modificarNovedadesById }
--- SITIO FINAL/routes/admin/novedades.js
+++ SITIO FINAL/routes/admin/novedades.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
 
 router.get('/', async function (req, res, next) {
-var novedades = await novedadesModel.getNovedades();
+    var novedades = await novedadesModel.getNovedades();
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -13,9 +13,74 @@ var novedades = await novedadesModel.getNovedades();
 
 router.get('/eliminar/:id', async (req, res, next) => {
     var id = req.params.id;
-    
+
     await novedadesModel.deleteNovedadesById(id);
     res.redirect('/admin/novedades');
 });
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        console.log(req.body)
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedades(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la Novedad'
+        })
+    }
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+
+    console.log(req.params.id);
+    var novedad = await novedadesModel.getNovedadesById(id);
+
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    });
+});
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        console.log(req.body);
+
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+        console.log(obj)
+
+        await novedadesModel.modificarNovedadesById(obj, req.body.id);
+        res.redirect('../novedades');
+
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la Novedad'
+        })
+    }
+})
 
 module.exports = router;
